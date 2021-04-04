2eb803b9ef23475f167f1fc5d9dc4e74a9ab033f
Igor de Oliveira Santana <igorsantana@alunos.utfpr.edu.br>
2021-04-04 12:41:48 -0300

    Ajustando detalhes mobile

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -27,7 +27,7 @@ function Navbar (props) {
                         <path fillRule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z"/>
                     </svg>
                 </Link>
-                <p style={{whiteSpace: 'nowrap', marginRight: '1rem'}} hidden={props.tab === 'Início' ? true : false}><strong>{useSelector(state => {
+                <p style={{whiteSpace: 'nowrap', marginRight: '1rem', fontSize: '1.3rem'}} hidden={props.tab === 'Início' ? true : false}><strong>{useSelector(state => {
                     switch(props.tab){
                         case 'Repositórios':
                             return `${state.public_repos} Repositórios`
